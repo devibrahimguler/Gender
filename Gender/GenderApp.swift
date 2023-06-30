a8200ad7b310e1fdd99b625ab91b485708745e1b
ibrahim <04190000633@ogrenci.ege.edu.tr>
2023-06-30 16:30:13 +0300

    Initial Commit

---
+++ Gender/GenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenderApp.swift
+//  Gender
+//
+//  Created by İbrahim Güler on 30.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct GenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
